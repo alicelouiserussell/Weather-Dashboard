b738724a07a26deef6648cdd7e95cee02d1a9c27
Alice Russell <alice.louise.russell@gmail.com>
2019-10-31 12:58:28 -0400

    current weather and uv apppends when searched

--- assets/script.js
+++ assets/script.js
@@ -7,17 +7,68 @@
 
 
 
-
+        
 var apiKey = "c3bb0467c5fe3c085d6f9dc0307b561b";
-
 
 $(document).ready(function(){
   
 $("#search-button").on("click", function(event){
     event.preventDefault();
     var cityInput = $("input").val();
+    var currentUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput},us&APPID=${apiKey}&units=imperial`
     console.log(cityInput);
+    console.log(currentUrl);
 
+    $.ajax({
+        url: currentUrl,
+        method: "GET"
+    }).done(function(response){
+        console.log(response.name);
+        console.log(response);
+        console.log(response.main.temp);
+        console.log(response.main.humidity);
+        console.log(response.wind.speed);
+        console.log(response.coord.lat);
+        console.log(response.coord.lon);
+
+        var currentCity = response.name;
+        var currentTemp = response.main.temp;
+        var currentHumidity = response.main.humidity;
+        var currentWind = response.wind.speed;
+        var currentLat = response.coord.lat;
+        var currentLon = response.coord.lon;
+
+        var currentUvUrl = `https://api.openweathermap.org/data/2.5/uvi?lat=${currentLat}&lon=${currentLon}&APPID=${apiKey}`;
+
+        $("#current-city").append(currentCity);
+        $("#current-temp").append(currentTemp + " °F");
+        $("#current-humidity").append(currentHumidity + " %");
+        $("#current-wind").append(currentWind + " MPH");
+
+        $.ajax({
+            url: currentUvUrl,
+            method: "GET"
+        }).done(function(response){
+            console.log(response);
+            console.log(response.value);
+            var currentUv = response.value;
+            $("#uv-index").append(currentUv);
+        });
+
+        // var fiveDayUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${currentCity},us&APPID=${apiKey}`;
+
+        // $.ajax({
+        //     url: fiveDayUrl,
+        //     method: "GET"
+        // }).done(function(response){
+        //     console.log(response);
+            
+        // });
+            
+
+    });   
+
+    
 });
 
 });
@@ -34,26 +85,7 @@ function searchHistory(){
 
 };
 
-function UvIndex (){
+function uvIndex (){
 
 };
 
-// $(".city-form").on("click", ".btn", function(event){
-//     event.preventDefault();
-//     alert("hello!");
-//     var cityInput = $(this).attr("data-city");
-//     console.log(cityInput);
-//     var currentUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput},us&APPID=${apiKey}&units=imperial`;
-//     console.log(cityInput);
-
-
-
-//     $.ajax({
-//         url: currentUrl,
-//         method: "GET"
-//     }).done(function(response){
-//         console.log(response);
-
-
-//     });
-// });
